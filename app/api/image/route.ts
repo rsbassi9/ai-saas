109210f00dce99fdce486f23e02aa572b324c418
rsbassi9 <rajvir.bassi95@gmail.com>
2023-09-05 05:53:40 -0700

    added code to check api limit and to deny access once limit is reached to all pages

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { OpenAI } from "openai";
 
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+
 require("dotenv").config();
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -35,11 +37,19 @@ export async function POST(req: Request) {
       return new NextResponse("Resolution is required", { status: 400 });
     }
 
+    const feeTrial = await checkApiLimit();
+
+    if (!feeTrial) {
+      return new NextResponse("Free trial has expired.", { status: 403 });
+    }
+
     const response = await openai.images.generate({
       prompt,
       n: parseInt(amount, 10),
       size: resolution,
     });
+
+    await increaseApiLimit();
 
     return NextResponse.json(response.data); //return NextResponse.json(response.data.data)
   } catch (error) {
