62100bec3736837dbafeb8e43be961a4a3f853a3
rsbassi9 <rajvir.bassi95@gmail.com>
2023-09-18 06:31:05 -0700

    Implemented functionality of "upgrade" button to direct to Stripe payment page

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import axios from "axios";
 import {
   Music,
   MessageSquare,
@@ -9,6 +10,7 @@ import {
   Check,
   Zap,
 } from "lucide-react";
+import { useState } from "react";
 
 import {
   Dialog,
@@ -60,6 +62,20 @@ const tools = [
 
 export const ProModal = () => {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/stripe");
+
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log(error, "STRIPE_CLIENT_ERROR");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -91,7 +107,12 @@ export const ProModal = () => {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button size="lg" variant="premium" className="w-full">
+          <Button
+            onClick={onSubscribe}
+            size="lg"
+            variant="premium"
+            className="w-full"
+          >
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
